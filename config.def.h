f1d8c97e8e05542f347449384605ee2327ed2c4c
salahdin-ahmed <salah2112004@gmail.com>
2020-11-20 13:01:29 +0200

    Reset configuration due to conflicts

--- config.def.h
+++ config.def.h
@@ -36,8 +36,8 @@ static Bool npisrelative  = False;
 #define MODKEY ControlMask
 static Key keys[] = {
 	/* modifier             key        function     argument */
-	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
-	{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
+	{ MODKEY|ShiftMask,     XK_t,      focusonce,   { 0 } },
+	{ MODKEY|ShiftMask,     XK_t,      spawn,       { 0 } },
 
 	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
 	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
